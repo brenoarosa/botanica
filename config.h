1dde0af1f777f876e73085c7114f0abd447ae35e
Breno Arosa <breno.arosa@cern.ch>
2015-11-29 18:52:53 -0200

    Adds a basic config file

---
+++ config.h
@@ -0,0 +1,12 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* network parameters */
+
+#define NODE_ADDR 0x1
+
+/* kernel configurations */
+
+#define MAX_ACTIONS 256
+#define MAX_REG_PROC 35
+#define MAX_DYN_PROC 20
