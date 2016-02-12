46218f4a811b015408e63e81d332d9f028f5f366
Breno Arosa <breno.arosa@twistsystems.com>
2016-02-12 18:46:54 -0200

    Fixs in network functions parameters

--- comm/network.h
+++ comm/network.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Wire.h>
+#include "includes.h"
 
 /*
  * @brief   Network setup
@@ -18,6 +19,6 @@ void setup_net(const addr_t node_addr, void (*message_handler)(int));
  * @param   msg - in - message to be spread to all nodes
  * @param   msg_len - in - message lenght
  */
-void send_msg(const uint8_t &msg, const size_t msg_len);
+void send_msg(const uint8_t *msg, const size_t msg_len);
 
-void send_msg(const addr_t node_addr, const uint8_t &msg, const size_t msg_len);
+void send_msg(const addr_t node_addr, const uint8_t *msg, const size_t msg_len);
