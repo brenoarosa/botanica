c8bd5ab3974dc05c0e3cf7cf330b784552b9de39
Breno Arosa <breno.arosa@twistsystems.com>
2016-02-20 12:45:17 -0200

    Includes humidity in kernel

--- botanica.ino
+++ botanica.ino
@@ -1,7 +1,18 @@
 #include "kernel.h"
 
 void setup() {
+  Serial.begin(57600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Setting up kernel.");
   setup_kernel();
+
+  Serial.println("Setting up Humidity module.");
+  setup_hum_controller();
+  setup_hum_sensor();
+
+  Serial.println("End kernel setup.");
 }
 
 void loop() {
